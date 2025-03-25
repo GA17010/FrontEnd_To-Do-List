7c473d5c02eae304de48e2d69323f9fd0a70724b
Gustavo Adolfo Gutierrez Aguirre <GA17010@ues.edu.sv>
2025-03-25 12:56:32 -0600

    refactor: replace environment variable API URL with a hardcoded public URL in api.ts

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -4,11 +4,11 @@ export interface Task {
   done: boolean;
 }
 
+const API_URL_PUBLIC = "https://backendto-do-list-production.up.railway.app";
+
 export const fetchTasks = async (): Promise<Task[]> => {
   try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_URL}/api/tasks/`
-    );
+    const response = await fetch(`${API_URL_PUBLIC}/api/tasks/`);
     return response.json();
   } catch (error) {
     console.error("Error fetching tasks", error);
@@ -17,25 +17,22 @@ export const fetchTasks = async (): Promise<Task[]> => {
 };
 
 export const addTask = async (task: Task): Promise<Task> => {
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/api/tasks/`,
-    {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ name: task.name, done: task.done }),
-    }
-  );
+  const response = await fetch(`${API_URL_PUBLIC}/api/tasks/`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ name: task.name, done: task.done }),
+  });
   return response.json();
 };
 
 export const deleteTask = async (id: number): Promise<void> => {
-  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tasks/${id}/`, {
+  await fetch(`${API_URL_PUBLIC}/api/tasks/${id}/`, {
     method: "DELETE",
   });
 };
 
 export const updateTask = async (task: Task): Promise<Task> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/tasks/${task.id}/`, {
+  const response = await fetch(`${API_URL_PUBLIC}/api/tasks/${task.id}/`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name: task.name, done: task.done }),
