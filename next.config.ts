5509e84ee41ba579f2d7c58b473afeb95d0f6a22
Gustavo Adolfo Gutierrez Aguirre <GA17010@ues.edu.sv>
2025-03-25 01:34:55 -0600

    refactor: remove API rewrites configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${process.env.REACT_APP_API_URL}/api/:path*`, // Cambia esto por la URL de tu backend
-      },
-    ];
-  },
 };
 
 export default nextConfig;
